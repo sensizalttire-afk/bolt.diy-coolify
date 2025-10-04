37eaaaa16d4031eda03e2009335c11519ea911fa
selcuq1616 <selcuq16@gmail.com>
2025-10-05 01:53:11 +0300

    vite-tsconfig-paths eklendi: Coolify deploy için '~' alias çözümü

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
-import remix from '@remix-run/dev/vite';
+import { vitePlugin } from '@remix-run/dev';
+import Unocss from 'unocss/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
+// virtual:uno.css hatasını çözmek için unocss pluginini ekliyoruz
+// '~' alias'ı için tsconfig yolunu çözüyoruz
 export default defineConfig({
-  plugins: [remix()],
+  plugins: [vitePlugin(), Unocss(), tsconfigPaths()],
   server: {
     host: true,  // 0.0.0.0 ile aynı şey
     port: 5173,
